9670a024c97b2a117c6cca50713a3944a80a1af9
Dmitry Sinyagin <itk_system@mail.ru>
2024-11-06 02:27:56 +0300

    061120240227

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -30,11 +30,22 @@ router.get('/order/create-succes', authMiddleware.authenticateToken, createOrder
 router.get('/order/create-error', authMiddleware.authenticateToken, createOrderError); // заказ не создан
 
 
+router.post('/api/auth/v1/logout', (req, res) => {
+    res.clearCookie('accessToken'); // Удаляем HttpOnly cookie
+    res.status(200).json({ message: 'Вы вышли из системы' });
+});
 
 router.post('/api/auth/v1/login', async function (request, response) {
     const { email, password } = request.body;
     const res = await authClient.login(email, password);
     if (res.success) {
+        // Отправляем cookie с флагами HttpOnly и Secure
+        response.cookie('accessToken', res.data.token, {
+             httpOnly: true,
+             secure: true, // Требуется HTTPS
+             sameSite: 'Strict', // Дополнительная защита от CSRF
+            maxAge: 3600000 // Время жизни токена (1 час)
+         });
         response.status(200).json(res.data);
     } else if (res.status) {
         response.status(res.status).json(res.data);
